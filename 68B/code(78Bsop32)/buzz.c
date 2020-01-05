148c51f6dd6671f15b7a9b46b93fd8e9f7de8c3c
2017115208_TenxMcu <1527846734@qq.com>
2020-01-05 10:59:44 +0800

    蜂鸣器

---
+++ 68B/code(78Bsop32)/buzz.c
@@ -0,0 +1,23 @@
+#define __buzz_c
+
+
+
+#include "includeAll.h"
+
+#include "buzz.h"
+
+void BuzzCount(uint8_t count)
+
+{	
+
+    buzzCounter = count ; 
+
+}
+
+
+
+void Buzz(){
+
+    buzzLastTimer = D_buzzLastTime ;
+
+}
